d9e916703f728716b15f3b6a2532297bdb83032b
ThisIsDaAccount <ramasp1998@live.com>
2018-04-30 19:08:17 -0700

    Update RUN_SETTINGS.java

--- RUN_SETTINGS.java
+++ RUN_SETTINGS.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class RUN_SETTINGS 
 {
-	public final static boolean DEBUG = true;
+	public final static boolean DEBUG = false;
 	private static PrintStream DEBUG_LOG;
 	
 	public static final PrintStream debug_log() throws FileNotFoundException
